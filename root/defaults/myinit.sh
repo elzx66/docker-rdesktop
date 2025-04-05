aa4b45815d67db57bff9e64c1a78cf4c896f56a4
elzx66 <dlecu@dingtalk.com>
2025-04-05 12:33:21 +0800

    Create myinit.sh

---
+++ root/defaults/myinit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 检查参数数量
+if [ $# -ne 2 ]; then
+    echo "用法: $0 <HOME_DIR> <PYCHARM_VERSION>"
+    exit 1
+fi
+
+# 获取参数
+HOME="$1"
+PYCHARM_VERSION="$2"
+
+# 检查桌面目录是否存在，如果不存在则创建
+if [ ! -d "${HOME}/Desktop" ]; then
+    mkdir -p "${HOME}/Desktop"
+fi
+
+# 复制 Chromium 和 WPS Office 的桌面快捷方式到桌面
+cp /usr/share/applications/chromium.desktop "${HOME}/Desktop/"
+cp /usr/share/applications/wps-office-prometheus.desktop "${HOME}/Desktop/"
+
+# 创建 PyCharm 的桌面快捷方式
+cat << EOF > ${HOME}/Desktop/pycharm.desktop
+[Desktop Entry]
+Name=PyCharm Community Edition
+Comment=Python IDE
+Exec=/opt/pycharm-community-${PYCHARM_VERSION}/bin/pycharm.sh
+Icon=/opt/pycharm-community-${PYCHARM_VERSION}/bin/pycharm.svg
+Terminal=false
+Type=Application
+Categories=Development;IDE;
+EOF
